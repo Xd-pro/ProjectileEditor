87e03a599f8125608f3fa826989950d9eabe799e
Finnbar M <62808970+Xd-pro@users.noreply.github.com>
2022-02-08 18:35:51 +0000

    remove debug stuff

--- src/listeners/ProjectileListener.php
+++ src/listeners/ProjectileListener.php
@@ -16,13 +16,13 @@ use pocketmine\item\ItemIds;
 class ProjectileListener implements Listener {
 
     public function onDamage(EntityDamageEvent $event) {
-        Main::$instance->getLogger()->info("damage detected");
+        //Main::$instance->getLogger()->info("damage detected");
         if ($event->getCause() == EntityDamageEvent::CAUSE_PROJECTILE && $event instanceof EntityDamageByChildEntityEvent) {
-            Main::$instance->getLogger()->info("projectile detected");
+            //Main::$instance->getLogger()->info("projectile detected");
             $child = $event->getChild();
             if (!$child instanceof Projectile) return;
             if (isset(Main::$instance->itemModifications[Main::NETWORK_ITEM[$child->getNetworkTypeId()]])) {
-                Main::$instance->getLogger()->info("modded projectile detected");
+                //Main::$instance->getLogger()->info("modded projectile detected");
                 $mod = Main::$instance->itemModifications[Main::NETWORK_ITEM[$child->getNetworkTypeId()]];
                 $event->setAttackCooldown($mod->getHitDelay());
             }
